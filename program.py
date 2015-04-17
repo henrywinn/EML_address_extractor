75c417af03f2e7bc2756d27a9ea921d8fb8be6d9
Henry Winn <henrywinn@henrywinn.com>
2015-04-17 16:19:52 -0500

    Handles strange events better and gets multiple addresses from single email with multiple recipients
    Strange events include:
    Doesn't open files that aren't .eml
    Doesn't break on files that don't have To field
    
    Reports files opened and addresses collected at end of execution
    
    This update changes the name of the address function from clean_ to get_addresses

--- program.py
+++ program.py
@@ -1,31 +1,44 @@
 from email.parser import Parser
 from email.message import Message
 from os import walk
-import re
+import re, sys
 
 ignore_to = raw_input('If you would like to filter out emails sent to you, enter your email: ')
 
 dictionary = {}
 parser = Parser()
 return_file = open('emails.csv', 'w')
+address_count = 0
+file_count = 0
 
-def clean_address(string):
-	regex = re.compile(r'[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}')
-	matches = regex.findall(string)
-	if len(matches) > 0:
-		return matches[0].lower()
-	else:
-		return False
+def get_addresses(string):
+    regex = re.compile(r'[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}')
+    if isinstance(string,basestring):
+        matches = regex.findall(string)
+        addresses = [x.lower() for x in matches]
+        if len(addresses) > 0:
+            return addresses
+        else:
+            return False
+    else:
+        return False
 
 for (dirpath, dirnames, filenames) in walk('emails'):
     for filename in filenames:
-    	f = open(dirpath+'/'+filename, 'r')
-    	eml = parser.parse(f, True)
-    	address = clean_address(eml.get('To'))
-    	if address != False and address != ignore_to: 
-    		if address in dictionary:
-    			print 'hit'
-    		else:
-    			dictionary[address] = True
-    			return_file.write(address+"\n")
-    			print address
+        if filename[-3:] == 'eml':
+            f = open(dirpath+'/'+filename, 'r')
+            file_count += 1
+            eml = parser.parse(f, True)
+            addresses = get_addresses(eml.get('To'))
+            if addresses != False:
+                for address in addresses:
+                    if address != ignore_to: 
+                        if address not in dictionary:
+                            dictionary[address] = True
+                            return_file.write(address+"\n")
+                            address_count += 1
+                            sys.stdout.write("\r%s files read" % file_count)
+            f.close()
+
+sys.stdout.write("\r%s files scanned\n" % file_count)
+sys.stdout.write("%s emails added to csv\n" % address_count)
